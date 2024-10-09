418d0a896531262c9d75ae79935e0e0d65c41643
Serge-Mane <sergemane81@gmail.com>
2024-10-09 08:15:12 +0000

    Les types de donnees javascript

---
+++ scripts/operation.js
@@ -0,0 +1,19 @@
+var entier;
+entier=10
+
+console.log(typeof entier,entier);
+
+var reel=10.45;
+console.log(typeof reel,reel);
+
+let chaineDeCarracteres="Samsam";
+console.log(typeof chaineDeCarracteres,chaineDeCarracteres);
+
+const PI=22/7;
+console.log(typeof PI, PI)
+
+const IS_TRAINER=true;
+console.log(typeof IS_TRAINER,IS_TRAINER);
+
+let numbers=[10,3,4,5];
+console.log(typeof numbers,numbers);
