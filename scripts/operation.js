bdd9bfd4d7115dd29be886503cd6de41f49b7062
Serge-Mane <sergemane81@gmail.com>
2024-10-09 09:30:38 +0000

    Listes des operations

--- scripts/operation.js
+++ scripts/operation.js
@@ -1,25 +1,9 @@
-/**
- * Formation javascript
- * pesentation des types de donnees
- * Var (DEPRECATED)
- * let pour creer
- */
+let resultat = 0;
+const firstNumber = 10;
+const secondNumber= 20;
 
-var entier;
-entier = 10;
-console.log(typeof entier, entier);
+resultat=firstNumber + secondNumber;
+console.log("La somme",resultat);
 
-var reel = 10.45;
-console.log(typeof reel, reel);
-
-let chaineDeCarracteres = "Samsam";
-console.log(typeof chaineDeCarracteres, chaineDeCarracteres);
-
-const PI = 22 / 7;
-console.log(typeof PI, PI);
-
-const IS_TRAINER = true;
-console.log(typeof IS_TRAINER, IS_TRAINER);
-
-let numbers = [10, 3, 4, 5];
-console.log(typeof numbers, numbers);
+resultat=firstNumber * secondNumber;
+console.log("Le produit",resultat);
