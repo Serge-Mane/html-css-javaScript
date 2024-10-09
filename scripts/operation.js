bff8cb3db83575b87d9c1cb6f5d1e1c9d716a184
Serge-Mane <sergemane81@gmail.com>
2024-10-09 09:08:01 +0000

    les commentaires

--- scripts/operation.js
+++ scripts/operation.js
@@ -1,19 +1,25 @@
+/**
+ * Formation javascript
+ * pesentation des types de donnees
+ * Var (DEPRECATED)
+ * let pour creer
+ */
+
 var entier;
-entier=10
+entier = 10;
+console.log(typeof entier, entier);
 
-console.log(typeof entier,entier);
+var reel = 10.45;
+console.log(typeof reel, reel);
 
-var reel=10.45;
-console.log(typeof reel,reel);
+let chaineDeCarracteres = "Samsam";
+console.log(typeof chaineDeCarracteres, chaineDeCarracteres);
 
-let chaineDeCarracteres="Samsam";
-console.log(typeof chaineDeCarracteres,chaineDeCarracteres);
+const PI = 22 / 7;
+console.log(typeof PI, PI);
 
-const PI=22/7;
-console.log(typeof PI, PI)
+const IS_TRAINER = true;
+console.log(typeof IS_TRAINER, IS_TRAINER);
 
-const IS_TRAINER=true;
-console.log(typeof IS_TRAINER,IS_TRAINER);
-
-let numbers=[10,3,4,5];
-console.log(typeof numbers,numbers);
+let numbers = [10, 3, 4, 5];
+console.log(typeof numbers, numbers);
