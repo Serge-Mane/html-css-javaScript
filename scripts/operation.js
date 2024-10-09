19657cc73c185624e3274c2485338acf000f9365
Serge-Mane <sergemane81@gmail.com>
2024-10-09 11:13:18 +0000

    Lecture du dom

--- scripts/operation.js
+++ scripts/operation.js
@@ -1,9 +1,23 @@
-let resultat = 0;
-const firstNumber = 10;
-const secondNumber= 20;
+//lire les contenu de la balise h1
 
-resultat=firstNumber + secondNumber;
-console.log("La somme",resultat);
+/**
+ * 
+getElementsByTagName: recuperer chacun des elements qui ont uun nom precis
+contentOfH1tags: recuperer le contenu de tous les h1.
+ */
+const contentOfH1tags=document.getElementsByTagName("h1");
 
-resultat=firstNumber * secondNumber;
-console.log("Le produit",resultat);
+/**
+ * Afficher le contenu des balises h1
+ */
+console.log(contentOfH1tags);
+
+/**
+ * le contenu de la premiere balise h1
+ */
+const firstH1=contentOfH1tags[0];
+console.log(firstH1.innerHTML);
+
+const button =document.getElementById("calculer");
+const buttonLabel=button.innerText;
+console.log(buttonLabel);
